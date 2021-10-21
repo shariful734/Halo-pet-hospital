a3bec0c5cc0fe8763c65a3dc2c437b5069e1d19a
shariful734 <shariful.sharifulrony@gmail>
2021-10-21 21:13:43 +0600

    all done

--- src/Pages/serviceDetail/ServiceDetail/ServiceDetails.js
+++ src/Pages/serviceDetail/ServiceDetail/ServiceDetails.js
@@ -1,9 +1,19 @@
 
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import SingleServiceDetail from '../SingleServiceDetail/SingleServiceDetail';
 
 
 const ServiceDetails = () => {
     const { serviceId } = useParams();
+
+    const [details, setDetails] = useState([])
+    useEffect(() => {
+        fetch('./FakeData.json')
+            .then(res => res.json())
+            .then(data => setDetails(data))
+
+    }, [])
 
 
 
@@ -11,8 +21,15 @@ const ServiceDetails = () => {
         <div className="container text-center">
             <h1 className="text-warning">Here is the detail of our sevice</h1>
 
-            <h3>{serviceId}</h3>
+            {
+                details.map(detail => <SingleServiceDetail
+                    key={detail.serviceName}
+                    detail={detail}
 
+                >
+
+                </SingleServiceDetail>)
+            }
 
         </div>
     );
